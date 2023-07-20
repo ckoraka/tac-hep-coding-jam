5297aaca894deec37477132184268415ca6f6922
Henry Schreiner <HenrySchreinerIII@gmail.com>
2023-07-20 09:19:33 -0400

    fix: download eigen & cleanup (#3)
    * fix: download eigen
    
    * fix: download Eigen & cleanup
    
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>
    
    ---------
    
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- src/tac_hep_coding_jam/_compat/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Copyright (c) 2023 ckoraka. All rights reserved.
-
-tac-hep-coding-jam: coding jam
-"""
-
-
-from __future__ import annotations
--- tests/test_compiled.py
+++ tests/test_compiled.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+import numpy as np
+import pytest
+
 import tac_hep_coding_jam._core as m
 
 
@@ -9,3 +12,7 @@ def test_add():
 
 def test_subtract():
     assert m.subtract(7, 5) == 2
+
+
+def test_matrix():
+    assert m.matrix(3) == pytest.approx(np.zeros((3, 3)))
