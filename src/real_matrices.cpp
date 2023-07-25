4767d53e0f6fdd8dc0de954cbe08ffac8b5193c1
Matt Kwiecien <mattkwiecien@gmail.com>
2023-07-25 13:34:02 -0700

    Changes from pre-commit

--- src/real_matrices.cpp
+++ src/real_matrices.cpp
@@ -2,29 +2,27 @@
 #include <pybind11/eigen.h>
 #include <pybind11/pybind11.h>
 
-
 namespace py = pybind11;
 
 PYBIND11_MODULE(real_matrices, m) {
 
-  m.def("eigenvectors", [](Eigen::MatrixXf matrix){
+  m.def("eigenvectors", [](Eigen::MatrixXf matrix) {
     Eigen::EigenSolver<Eigen::MatrixXf> es(matrix);
     return es.eigenvectors();
   });
 
-  m.def("eigenvalues", [](Eigen::MatrixXf matrix){
+  m.def("eigenvalues", [](Eigen::MatrixXf matrix) {
     Eigen::EigenSolver<Eigen::MatrixXf> es(matrix);
     return es.eigenvalues();
   });
 
-    m.def("eigenvectors", [](Eigen::MatrixXd matrix){
+  m.def("eigenvectors", [](Eigen::MatrixXd matrix) {
     Eigen::EigenSolver<Eigen::MatrixXd> es(matrix);
     return es.eigenvectors();
   });
 
-  m.def("eigenvalues", [](Eigen::MatrixXd matrix){
+  m.def("eigenvalues", [](Eigen::MatrixXd matrix) {
     Eigen::EigenSolver<Eigen::MatrixXd> es(matrix);
     return es.eigenvalues();
   });
-
 }
