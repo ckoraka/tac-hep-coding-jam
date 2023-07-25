b41a25dddda423354f9723d4c1f4c5c99ce58ef2
jmw464 <johannes@wagner.cc>
2023-07-25 16:56:30 -0400

    Fixed formatting

--- performance/timing.py
+++ performance/timing.py
@@ -1,17 +1,26 @@
-import scipy.linalg
-import numpy as np
+from __future__ import annotations
+
 import timeit
 
-#takes in a function and a list of matrix sizes N (for NxN matrices) and tests performance for each matrix
-def timing(func, sizes):
-    stats = np.zeros((len(sizes), 3)) #N, mean execution time, std execution time
+import numpy as np
+import scipy.linalg
+
+
+# takes in a function and a list of matrix sizes N (for NxN matrices) and tests performance for each matrix
+def calc_timing(func, sizes):
+    stats = np.zeros((len(sizes), 3))  # N, mean execution time, std execution time
 
     for i, n in enumerate(sizes):
         matrix = np.random.rand(n, n)
-        sym_pos_def = 0.5*(matrix + matrix.T) + n*np.identity(n) #ensure matrix is symmetric positive definite
+        sym_pos_def = 0.5 * (matrix + matrix.T) + n * np.identity(
+            n
+        )  # ensure matrix is symmetric positive definite
         time = timeit.repeat(lambda: func(sym_pos_def), repeat=10000, number=1)
         stats[i] = [n, np.mean(time), np.std(time)]
 
     return stats
 
-cholesky_performance = timing(lambda x: scipy.linalg.cholesky(x, lower=True), range(10,100,10))
+
+cholesky_performance = calc_timing(
+    lambda x: scipy.linalg.cholesky(x, lower=True), range(10, 100, 10)
+)
