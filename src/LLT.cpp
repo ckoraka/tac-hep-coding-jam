e99cc2f8ded72881b0ce4ad6a8124f2a04316e37
Matt Maroun <mmaroun@umass.edu>
2023-07-26 16:24:51 -0400

    Further edits to debug LLT.cpp. Now added verbosity to funcion to avoid memory errors

--- src/LLT.cpp
+++ src/LLT.cpp
@@ -1,4 +1,5 @@
 #include <Eigen/Dense>
+#include <iostream>
 #include <pybind11/eigen.h>
 #include <pybind11/pybind11.h>
 
@@ -18,7 +19,11 @@ PYBIND11_MODULE(_eigenLLT, m) {
   m.def(
       "computeAndSolve",
       [](Eigen::MatrixXf a, Eigen::MatrixXf b) {
+        std::cout << "Here is the matrix A:\n" << a << std::endl;
+        std::cout << "Here is the right hand side b:\n" << b << std::endl;
+        std::cout << "Computing LLT decomposition..." << std::endl;
         foo.compute(a);
+        std::cout << "The solution is:\n" << foo.solve(b) << std::endl;
         return foo.solve(b);
       },
       "Compute Eigenvales of Matrix A to solve b = Ax problem. A = LL^T");
@@ -26,7 +31,11 @@ PYBIND11_MODULE(_eigenLLT, m) {
   m.def(
       "computeAndSolve",
       [](Eigen::MatrixXd a, Eigen::MatrixXd b) {
+        std::cout << "Here is the matrix A:\n" << a << std::endl;
+        std::cout << "Here is the right hand side b:\n" << b << std::endl;
+        std::cout << "Computing LLT decomposition..." << std::endl;
         bar.compute(a);
+        std::cout << "The solution is:\n" << bar.solve(b) << std::endl;
         return bar.solve(b);
       },
       "Compute Eigenvales of Matrix A to solve b = Ax problem. A = LL^T");
