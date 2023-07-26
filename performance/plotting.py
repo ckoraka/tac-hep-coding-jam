eda141025a289d94f4bc0ea6899ba85e57e165d5
twnelson0 <twnelson2@wisc.edu>
2023-07-26 16:13:40 -0400

    Added script that generates performance plots

--- performance/plotting.py
+++ performance/plotting.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 
 # Takes an array of outputs from the timing function, a corresponding label array for the legend and a plot name
 def plotting(timing_arr, label_arr, fig_name):
-    # fig = plt.figure()
     for timing, label in zip(timing_arr, label_arr):  # Add each set of data to plot
         plt.errorbar(
             timing[:, 0],
@@ -18,6 +17,6 @@ def plotting(timing_arr, label_arr, fig_name):
 
     plt.xlabel("Matrix size (n)")
     plt.ylabel("Time (s)")
-    plt.ticklabel_format(axis="y", style="sci", scilimits=(0, 5))
+    plt.ticklabel_format(axis="y", style="sci", scilimits=(0, 2))
     plt.legend(loc="lower right")
     plt.savefig(fig_name)
