b98fa4ae4bcad767f639c010d1c1f8ee695b6185
Trevor Nelson <43486832+twnelson0@users.noreply.github.com>
2023-07-26 17:24:02 -0400

    Twnelson0/test performance (#49)
    * Pull request test
    
    * Added performance script for eigh/SelfAdjointEigenSolver
    
    * Added function to study timing performance
    
    * Changed tabs to spaces
    
    * Fixed parentheses
    
    * Fixed formatting
    
    * Fixed lambda function ambiguity
    
    * Added plotting script to compare performance of functions
    
    * Added script that generates performance plots
    
    * Removed unused blocks of code from selfadj_perf.py
    
    * Removed unused varaible
    
    ---------
    
    Co-authored-by: jmw464 <johannes@wagner.cc>

---
+++ performance/plotting.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+import matplotlib.pyplot as plt
+
+
+# Takes an array of outputs from the timing function, a corresponding label array for the legend and a plot name
+def perf_plotting(timing_arr, label_arr, fig_name):
+    for timing, label in zip(timing_arr, label_arr):  # Add each set of data to plot
+        plt.errorbar(
+            timing[:, 0],
+            timing[:, 1],
+            yerr=timing[:, 2],
+            marker="o",
+            linestyle="None",
+            label=label,
+        )
+
+    plt.xlabel("Matrix size (n)")
+    plt.ylabel("Time (s)")
+    plt.ticklabel_format(axis="y", style="sci", scilimits=(0, 2))
+    plt.legend(loc="lower right")
+    plt.savefig(fig_name)
