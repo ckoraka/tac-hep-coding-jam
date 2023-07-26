871514ab7f5d848e5c5614d4493c47359030dc29
twnelson0 <twnelson2@wisc.edu>
2023-07-26 17:08:14 -0400

    Removed unused varaible

--- performance/selfadj_perf.py
+++ performance/selfadj_perf.py
@@ -6,7 +6,6 @@ from timing import calc_timing
 
 if __name__ == "__main__":
     numpy_stats1 = calc_timing((lambda x: linalg.eigh(x)), range(10, 100, 10))
-    # numpy_stats2 = calc_timing((lambda x: linalg.eigh(x)), range(10, 100, 10))
     stats_arr = [numpy_stats1]
 
     perf_plotting(stats_arr, ["numpy.linalg.eigh()"], "TestPlot")
