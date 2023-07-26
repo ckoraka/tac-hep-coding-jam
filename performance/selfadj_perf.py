76150af9b8bb1c932bdc22b0e402600875f6fce1
twnelson0 <twnelson2@wisc.edu>
2023-07-26 17:02:20 -0400

    Removed unused blocks of code from selfadj_perf.py

--- performance/selfadj_perf.py
+++ performance/selfadj_perf.py
@@ -5,17 +5,8 @@ from plotting import perf_plotting
 from timing import calc_timing
 
 if __name__ == "__main__":
-    # print(timeit.repeat(timer=t,repeat = 1000000, number = 1))
-    # sig2 = np.array([[0,-1j],[1j,0]])
-    # time_arr = timeit.repeat(lambda: eighbench_mark(sig2),repeat=1000000,number=1)
-    # print(np.mean(time_arr))
-
     numpy_stats1 = calc_timing((lambda x: linalg.eigh(x)), range(10, 100, 10))
-    numpy_stats2 = calc_timing((lambda x: linalg.eigh(x)), range(10, 100, 10))
+    # numpy_stats2 = calc_timing((lambda x: linalg.eigh(x)), range(10, 100, 10))
     stats_arr = [numpy_stats1]
 
     perf_plotting(stats_arr, ["numpy.linalg.eigh()"], "TestPlot")
-
-# plt.plot(timeArr,'*')
-# plt.savefig("TestPlot.png")
-# print(timeit.timeit(lambda: eighbench_mark(), globals=globals()))
