42774bf1dafc8d299913909e854fd57717cc7277
jmw464 <johannes@wagner.cc>
2023-07-26 17:36:07 -0400

    Added LLT performance function

---
+++ performance/llt_perf.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from plotting import perf_plotting
+from timing import calc_timing
+import scipy
+
+if __name__ == "__main__":
+    numpy_stats1 = calc_timing((lambda x: scipy.linalg.cholesky(x, lower=True)), range(10, 100, 10), 10000)
+    stats_arr = [numpy_stats1]
+
+    perf_plotting(stats_arr, ["scipy.linalg.cholesky()"], "TestPlot")
