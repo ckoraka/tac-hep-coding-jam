ee70a4556a92a98259d2cab9015d16a67df6fca1
Russell Marroquin Solares <rmarroqu@cmslpc102.fnal.gov>
2023-07-28 11:32:01 -0500

    GenSelfAdjEigenSolver using numpy.

--- performance/timingGenSelfAdjEigenSolver.py
+++ performance/timingGenSelfAdjEigenSolver.py
@@ -16,13 +16,9 @@ def calc_timing(func, sizes):
         matrix = rng.random(size=(n, n))
         sym_pos_def = 0.5 * (matrix + matrix.T) + n * np.identity(n)  # ensure matrix is symmetric positive definite
         time = timeit.repeat(lambda sym_pos_def=sym_pos_def: func(sym_pos_def),  repeat=10000,number=1)
-        
         stats[i] = [n, np.mean(time), np.std(time)]
 
     return stats
 
 results = calc_timing(lambda x: LA.eig(x), range(10, 50, 10))
-
-
 plotting.perf_plotting([results], ["Eigensolver"], "TestPlot")
-
